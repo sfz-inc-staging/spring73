dbf7082bd8f698c3ebb27bbe85eb0b46829e208e
Ryan Day <jrday@users.noreply.github.com>
2018-08-22 09:31:58 -0700

    Update Spring73Controller.java

--- src/main/java/com/a/Spring73Controller.java
+++ src/main/java/com/a/Spring73Controller.java
@@ -1,5 +1,5 @@
 package com.a;
-
+ 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
