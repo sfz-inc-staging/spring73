6ae0c3e080fbaf04ef9e4f2a606e94f580422970
Ryan Day <jrday@users.noreply.github.com>
2018-08-22 09:25:27 -0700

    Initial commit from Atomist

---
+++ src/main/java/com/a/Spring73Application.java
@@ -0,0 +1,12 @@
+package com.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring73Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring73Application.class, args);
+	}
+}
